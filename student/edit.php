a95cd14284a0eebbd649298ece85a79d1510060c
Mentoring-MITS <85401285+Mentoring-MITS@users.noreply.github.com>
2021-06-08 20:17:28 +0530

    Update edit.php
    minor fix

--- student/edit.php
+++ student/edit.php
@@ -248,7 +248,7 @@
 		
 		<div class="flex items-center justify-between float-right">
 						<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" name="edit" type="submit">
-							Edit
+							Save
 						</button>
 			</div>
 		</form>
